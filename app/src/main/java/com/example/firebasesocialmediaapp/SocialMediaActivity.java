8ba0af45a2bcd006255edca560aca69509418eee
abhibawa <bawa3052000@gmail.com>
2020-05-17 15:22:19 +0530

    request permission and select picture for uploading

--- app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
+++ app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
@@ -2,10 +2,21 @@ package com.example.firebasesocialmediaapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +24,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SocialMediaActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private ImageView postImageView;
+    private Button btnCreatePost;
+    private EditText edtDes;
+    private ListView usersListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +35,28 @@ public class SocialMediaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_social_media);
 
         mAuth = FirebaseAuth.getInstance();
+        postImageView = findViewById(R.id.postImageView);
+        btnCreatePost = findViewById(R.id.btnCreatePost);
+        edtDes = findViewById(R.id.edtDes);
+        usersListView = findViewById(R.id.usersListView);
+
+        postImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectImage();
+
+            }
+        });
+
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
 
     }
 
@@ -49,10 +86,27 @@ public class SocialMediaActivity extends AppCompatActivity {
         super.onBackPressed();
         logout();
     }
-
     public void logout(){
         mAuth.signOut();
         finish();
         Toast.makeText(this, "Logged Out", Toast.LENGTH_LONG).show();
+    }
+
+    public void selectImage(){
+
+        if(Build.VERSION.SDK_INT < 23){
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 1000);
+        } else if(Build.VERSION.SDK_INT >=23){
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+            }else{
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1000);
+            }
+
+        }
+
     }
 }
